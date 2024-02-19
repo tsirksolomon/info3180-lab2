934cdb1fa8bc315f32fccc427d8930759a305c5c
Solomon <81330585+tsirksolomon@users.noreply.github.com>
2024-02-19 10:52:17 -0500

    Added profile.

--- app/views.py
+++ app/views.py
@@ -17,6 +17,11 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile():
+    """Render the website's profile page."""
+    return render_template('profile.html')
+
 
 ###
 # The functions below should be applicable to all Flask apps.
